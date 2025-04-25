b12b6c8eaf54bc167e67586d4ad8256581324ca7
Lucas Pacheco <lucasppmc@gmail.com>
2025-04-25 01:51:53 -0300

    All responsive bugs fixed

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -4,7 +4,6 @@ export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
-  // simple client-side validation
   if (!validateString(senderEmail, 500)) {
     return {
       error: "Invalid sender email",
--- components/intro.tsx
+++ components/intro.tsx
@@ -59,7 +59,7 @@ export default function Intro() {
         </div>
 
         <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xlmb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl max-w-[25rem] lg:max-w-full md:max-w-full sm:max-w-full"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         >
